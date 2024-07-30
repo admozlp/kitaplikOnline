c19fe3367b783f3eedc363b8f24409df9040e84e
Adem Özalp <75789726+admozlp@users.noreply.github.com>
2024-07-31 01:08:08 +0300

    Update book-service-dev.properties

--- library-service/src/main/java/com/ademozalp/library_service/controller/LibraryController.java
+++ library-service/src/main/java/com/ademozalp/library_service/controller/LibraryController.java
@@ -4,13 +4,18 @@ package com.ademozalp.library_service.controller;
 import com.ademozalp.library_service.dto.AddBookRequest;
 import com.ademozalp.library_service.dto.LibraryDto;
 import com.ademozalp.library_service.service.LibraryService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/libraries")
+@RefreshScope
 public class LibraryController {
+    @Value("${library.service.count}")
+    private String count;
 
     private final LibraryService libraryService;
 
@@ -34,5 +39,8 @@ public class LibraryController {
         return ResponseEntity.ok(libraryService.getAllBooksByLibraryId(id));
     }
 
-
+    @GetMapping("/count")
+    public ResponseEntity<String> getCount(){
+        return ResponseEntity.ok("Library service count is: " + count);
+    }
 }
